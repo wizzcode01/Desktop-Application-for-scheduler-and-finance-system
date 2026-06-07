309a7a0b72c975dc912e07c940c000dfd97f7448
Wisdom Dickson <dicksonwisdom005@gmail.com>
2026-06-07 15:38:28 +0100

    added apache license

--- src/main/java/com/example/scheduleapp/Main.java
+++ src/main/java/com/example/scheduleapp/Main.java
@@ -1,3 +1,12 @@
+/*
+ * wiSchedule — Task & Finance Manager
+ * Copyright 2026 Wisdom Edemekong Dickson
+ *
+ * Licensed under the Apache License, Version 2.0
+ * You may not use this file except in compliance with the License.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 package com.example.scheduleapp;
 
 import javafx.application.Application;
